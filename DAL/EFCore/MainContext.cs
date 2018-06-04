cc565b6e551b5d48353f03f50847c61b63ad3b94
Avidness <adavidness@gmail.com>
2018-06-03 20:34:12 -0500

    Add EF context for accessing item db

---
+++ DAL/EFCore/MainContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using notes_api.Models.Domain;
+
+namespace notes_api.DAL.EFCore
+{
+    public class MainContext : DbContext
+    {
+        public MainContext(DbContextOptions<MainContext> options) : base(options)
+        { }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
