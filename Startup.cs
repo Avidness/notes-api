a7fbccc2c93dbe188ed80391203bb48f240e05f9
Avidness <adavidness@gmail.com>
2018-06-03 20:37:15 -0500

    Handle json to model transformation, use DI for ItemRepository and the EF context

--- Startup.cs
+++ Startup.cs
@@ -4,12 +4,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
+using notes_api.DAL.EFCore;
+using notes_api.DAL.Repositories;
+using notes_api.Models.Domain;
 
-namespace toolshed_api
+namespace notes_api
 {
     public class Startup
     {
@@ -23,7 +27,15 @@ namespace toolshed_api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc().AddJsonOptions(options =>
+            {
+                options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            });
+            
+            services.AddSingleton<IItemRepository, ItemRepository>();
+            services.AddDbContext<MainContext>(options => 
+                options.UseSqlServer(Configuration["DefaultConnection"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
