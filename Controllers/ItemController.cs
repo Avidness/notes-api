bf73d70a0abafccb309495854e27c7e4741194e4
Avidness <adavidness@gmail.com>
2018-06-03 20:35:45 -0500

    REST API controller for items

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using notes_api.DAL.Repositories;
+using notes_api.Models.Domain;
+
+namespace notes_api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ItemController : Controller
+    {
+        public IItemRepository _items;
+
+        public ItemController(IItemRepository items)
+        {
+            _items = items;
+        }
+
+        [HttpGet]
+        public Task<IEnumerable<Item>> GetAll()
+        {
+            return _items.GetAll();
+        }
+
+        [HttpGet("{id}", Name = "GetItems")]
+        public IActionResult GetById(int id)
+        {
+            var item = _items.Get(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromForm] Item item)
+        {
+            if (item == null)
+                return BadRequest();
+
+            item.CreatedAt = DateTime.UtcNow;
+            _items.Create(item);
+            return new ObjectResult(item.Id);
+        }
+        
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromForm] Item item)
+        {
+            if (item == null || item.Id != id)
+                return BadRequest();
+
+            _items.Update(item);
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _items.Delete(id);
+            return new NoContentResult();
+        }
+    }
+}
