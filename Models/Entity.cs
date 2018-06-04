525a934e1cd379a0c447c6fd9f38c17730afdf95
Avidness <adavidness@gmail.com>
2018-06-03 20:31:23 -0500

    Add item entity

---
+++ Models/Entity.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace notes_api.Models.Domain
+{
+    public class Entity
+    {
+        [Key]
+        public virtual int Id { get; set; }
+
+        [DisplayName("Created At")]
+        public virtual DateTime CreatedAt { get; set; }
+
+        [DisplayName("Last Modified At")]
+        public virtual DateTime LastModifiedAt { get; set; }
+    }
+}
