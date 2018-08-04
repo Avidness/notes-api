a15c91a7e5ec178b875cc072348493ade7a7bdd4
Avidness <adavidness@gmail.com>
2018-08-04 12:21:59 -0500

    Add API endpoint for Category CRUD

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using notes_api.DAL.Repositories;
+using notes_api.Models.Domain;
+
+namespace notes_api.Controllers
+{
+    [Route("api/[controller]")]
+    public class CategoryController : Controller
+    {
+        public CategoryRepository _categories;
+
+        public CategoryController(CategoryRepository categories)
+        {
+            _categories = categories;
+        }
+
+        [HttpGet]
+        public Task<IEnumerable<Category>> GetAll()
+        {
+            return _categories.GetAll();
+        }
+
+        [HttpGet("{id}", Name = "GetCategories")]
+        public IActionResult GetById(int id)
+        {
+            var category = _categories.Get(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(category);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Category category)
+        {
+            if (category == null)
+                return BadRequest();
+
+            category.CreatedAt = DateTime.UtcNow;
+            _categories.Create(category);
+            return new ObjectResult(category);
+        }
+        
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody] Category category)
+        {
+            if (category == null || category.Id != id)
+                return BadRequest();
+
+            _categories.Update(category);
+            return new ObjectResult(category);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _categories.Delete(id);
+            return new NoContentResult();
+        }
+    }
+}
