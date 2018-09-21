8c49055c4485b109a8c9b419c6a6d08245edf74a
Avidness <adavidness@gmail.com>
2018-09-21 15:39:12 -0500

    Add test project

---
+++ notes-api-tests/UnitTests.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using notes_api.Controllers;
+
+namespace Tests
+{
+    [TestFixture] 
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
